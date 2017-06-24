338a2aafbbc4f4b5bda70f331aae0e59953ac5f0
Jackson Huang <huang.jizhou@wustl.edu>
2017-06-23 19:58:28 -0500

    module update

--- calculator.php
+++ calculator.php
@@ -8,22 +8,28 @@ Result:
 $in_a=$_GET['inputa'];
 $in_b=$_GET['inputb'];
 $method=$_GET['method'];
-switch ($method) {
 
-case "add":
-	echo ($in_a + $in_b);
-	break;
-case "sub":
-	echo ($in_a - $in_b);
-	break;
-case "mul":
-	echo ($in_a * $in_b);
-	break;
-case "div":
-	echo ($in_a / $in_b);
-	break;
-default:
-	break;
+if ($in_b == 0 && $method == 'div') {
+	echo "divisor cannot be 0!";
+}
+else {
+	switch ($method) {
+
+	case "add":
+		echo ($in_a + $in_b);
+		break;
+	case "sub":
+		echo ($in_a - $in_b);
+		break;
+	case "mul":
+		echo ($in_a * $in_b);
+		break;
+	case "div":
+		echo ($in_a / $in_b);
+		break;
+	default:
+		break;
+	}
 }
 ?>
 </body>
