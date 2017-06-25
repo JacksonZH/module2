4a20ebbfb43b55d74e13973afeba42bca5668463
Jackson Huang <huang.jizhou@wustl.edu>
2017-06-25 17:12:32 -0500

    update

--- open.php
+++ open.php
@@ -31,16 +31,16 @@
 	if (array_key_exists($uname, $_SESSION)) { 
 		echo "if";
 		// Normally, if the key exists, the value must not be nonempty, this is just a double check
-		if (!empty($_SESSION[$uname])) {  
-			$user = $_SESSION[$uname];
-			// check user list
-			if (!strpos(file_get_contents('~/users/UserList.txt'), $user)) {
-				redirect();
-			}
-		}
-		else {
-			redirect();
-		}
+		// if (!empty($_SESSION[$uname])) {  
+		// 	$user = $_SESSION[$uname];
+		// 	// check user list
+		// 	if (!strpos(file_get_contents('~/users/UserList.txt'), $user)) {
+		// 		redirect();
+		// 	}
+		// }
+		// else {
+		// 	redirect();
+		// }
 	}
 	// if the key does not exist, this access to open.php must be the first time
 	elseif (!array_key_exists($uname, array) && !empty($GET[$uname])) { 
@@ -55,11 +55,11 @@
 	// 	// $_SESSION[$dpath] = array();
 	// 	$_GET[$fname] = $_SESSION[$uname];
 	// 	unset($_GET[$uname]);
-		redirect();
+		// redirect();
 	}
 	else {
 		echo "else";
-		redirect();
+		// redirect();
 	}
 ?>
 
