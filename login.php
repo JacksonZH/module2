6fadb9b4f28e80c3770eb6c97d6b08f994384a14
Jackson Huang <huang.jizhou@wustl.edu>
2017-06-25 18:13:43 -0500

    new file

---
+++ login.php
@@ -0,0 +1,22 @@
+<?php
+session_unset();
+?>
+<!DOCTYPE html>
+
+<html>
+<head>
+<title>	login </title>
+
+</head>
+<body>
+<form action="info.php"  method ="get">
+  Username:<br>
+  <input type="text" name="username" />
+  <br>
+  <br>
+  <br>
+  <br> <input type= "submit" value= "submit" />
+</form> 
+
+</body>
+</html>
