fb2fb24f406ec3df1d61f29a55fd3a34503398b6
Jackson Huang <huang.jizhou@wustl.edu>
2017-06-25 08:40:09 -0500

    update

--- open.php
+++ open.php
@@ -42,7 +42,7 @@
 		}
 	}
 	// if the key does not exist, this access to open.php must be the first time
-	elseif (!array_key_exists($uname, array) && !empty($GET[$uname])) { 
+	elseif (!array_key_exists($uname, array) ) { 
 		$user = $_GET[$uname];
 		// check whether the username matches the names in the user list
 		if (!strpos(file_get_contents('~/users/UserList.txt'), $user)) {
