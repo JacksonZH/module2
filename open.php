2090edfdba10b07686bfd042d314bc4a14d60f6c
Jackson Huang <huang.jizhou@wustl.edu>
2017-06-25 08:04:39 -0500

    update

--- open.php
+++ open.php
@@ -1,36 +1,61 @@
-<!--  This file retrieves directory path from session variable: dirpath
-				retrieves file name from seesion variable     : filename
-				updates directory if filename is actually folder -->
+<!--  
+	This file is designed for accessing files and directory,
+	which will directly interact with all other PHP files. 
+	In particular, it gives the links of files ,directories
+	as well as buttons for uploading files and constructing
+	new folders. 
+
+	The file also get the user info from login.php and check 
+	its correctness. If the info is correct, it will list 
+	the user's contents with presenting them hierachically, 
+	otherwise, redirect to the login.php.
+
+	It retrieves username from session variable      :     username
+	   retrieves directory path from session variable:     directorypath
+	   retrieves file name from seesion variable     :     filename
+-->
 
 <?php
 	session_start();
+	// redirect to login page with indicating the identity validation failed
 	function redirect() {
-		header("Location: http://ec2-13-58-219-145.us-east-2.compute.amazonaws.com/~jackson/module2/trunk/login.php");
+		header("Location: http://ec2-13-58-219-145.us-east-2.compute.amazonaws.com/~jackson/module2/trunk/login.php?info=invalid");
 		exit();
 	}
-
+	// username
+	$uname = "username";
+	$dpath = "directorypath";
+	$fname = "filename";
+name
 	// check current permission status
-	if ($_SESSION['username'] == NULL) {
-		if ($_GET['username'] !== NULL) {
-			$user = $_GET['username'];
+	if (array_key_exists($uname, $_SESSION)) { 
+		// Normally, if the key exists, the value must not be nonempty, this is just a double check
+		if (!empty($_SESSION[$uname])) {  
+			$user = $_SESSION[$uname];
 			// check user list
-			if (strpos(file_get_contents('~/users/UserList.txt'), $user) == FALSE) {
+			if (!strpos(file_get_contents('~/users/UserList.txt'), $user)) {
 				redirect();
 			}
-			// initialize session variables
-			$_SESSION['username'] = $user;
-			$_SESSION['dirpath'] = array();
-			$_SESSION['filename'] = $user;
+		}
+		else {
+			redirect();
 		}
 	}
-	else {
-		$user = $_SESSION['username'];
-		// check user list
+	// if the key does not exist, this access to open.php must be the first time
+	elseif (!array_key_exists($uname, array) && !empty($GET[$uname])) { 
+		$user = $_GET[$uname];
+		// check whether the username matches the names in the user list
 		if (strpos(file_get_contents('~/users/UserList.txt'), $user) == FALSE) {
 			redirect();
 		}
-		// update session variable
-		$_SESSION['filename'] = $_GET['filename'];
+		// initialize session variables
+		$_SESSION[$uname] = $user;
+		$_SESSION[$dpath] = array();
+		$_GET[$fname] = $_SESSION[$uname];
+		unset($_GET[$uname]);
+	}
+	else {
+		redirect();
 	}
 ?>
 
@@ -41,26 +66,50 @@
 </head>
 <body>
 	<?php
-		// functions
-		function getPath($sessionVar) {
-			$userName = $sessionVar['username'];
+		// form the path of current directory using directory names 
+		// stored in session variable
+		function getDirPath() {
+			// refer the global vars
+			$uname = $GLOBALS['uname'];
+			$dpath = $GLOBALS['dpath'];
+
+			$userName = $_SESSION[$uname];
 			$dirPath = "~/users/".$userName;			
-			$pathArray = $sessionVar['dirpath'];
+			$pathArray = $_SESSION[$dpath];
 			foreach ($pathArray as $value) {
-				# form path
+				// form path
 				$dirPath .= "/".$value;
 			}
 			return $dirPath;
 		}
 
-		function listContent($dirPath) {
+		// form the path of the given file
+		function getFilePath() {
+			// refer the global vars
+			$fname = $GLOBALS['fname'];
+
+			$fileName = $_GET[$fname];
+			$dirPath = getDirPath();
+			return $dirPath."/".$fileName;
+		}
+
+		// this function will list all the files and directories 
+		// under current path by presenting them as link form
+		function listContent() {
+			$dirPath = getDirPath();
 			$items = scandir($dirPath);
-			if ($items !== FALSE) {
-				// it the folder is not empty
+			// check if, in current repo, we need a return link
+			if (!empty($_SESSION[$dpath])) {
+				if ($_SESSION[$uname] !== end($_SESSION[$dpath])) {   
+					echo "<a href=\"http://ec2-13-58-219-145.us-east-2.compute.amazonaws.com/~jackson/module2/trunk/open.php?filename=\">..</a><br>";
+				}
+			}
+			// check if current folder contain any item 
+			if (!empty($items)) {
+				// if nonempty, traverse the folder
 				foreach ($items as $value) {
-					//traverse the folder
-					$newPath = $dirPath."/".$value;
-					if (is_dir($newPath) == TRUE) {
+					$itemPath = $dirPath."/".$value;
+					if (is_dir($itemPath)) {
 						echo "<a href=\"http://ec2-13-58-219-145.us-east-2.compute.amazonaws.com/~jackson/module2/trunk/open.php?filename=".$value."\">".$value."/</a><br>";
 					}
 					else{
@@ -70,7 +119,10 @@
 			}
 		}
 
-		function openFile($filePath) {
+		// this function will open selected file with original form
+		// which means if the file is .pdf, it will be viewed as PDF 
+		function openFile() {
+			$filePath = getFilePath();
 			$finfo = finfo_open(FILEINFO_MIME_TYPE); // return mime type ala mimetype extension
 			if (!$finfo) {
 			 	echo "Opening fileinfo database failed";
@@ -83,40 +135,25 @@
 			readfile($filePath);
 		}
 
-		function backWard() {
-			if ($_SESSION['username'] !== end($_SESSION['dirpath'])) {   
-				echo "<a href=\"http://ec2-13-58-219-145.us-east-2.compute.amazonaws.com/~jackson/module2/trunk/open.php?filename=NULL\">..</a><br>";
+		// check if returned
+		if (empty($_GET[$fname])) {
+			if (end($_SESSION[$dpath]) !== $_SESSION[$uname]) {
+				$_GET[$fname] = array_pop($_SESSION[$dpath]);
 			}
+			listContent();
 		}
-
-		// return to the last repo
-		if ($_SESSION['filename'] == NULL) {
-			// update session variable
-			array_pop($_SESSION['dirpath']);
-			// generate new path
-			$dirPath = getPath($_SESSION);
-			// whether there should exist a go back option
-			backWard();
-			// show current folder 
-			listContent($dirPath);
+		elseif (file_exists(getFilePath())) {
+			if (is_dir(getFilePath())) {
+				array_push($_SESSION[$dpath], $_GET[$fname]);
+				listContent();
+			}
+			else {
+				openFile();
+			}
 		}
 		else {
-			// update session variable
-			array_push($_SESSION['dirpath'], $filename);
-			// generate new path
-			$dirPath = getPath($_SESSION);
-			// type of directory
-			if (is_dir($dirPath) == TRUE) {
-				// whether there should exist a go back option
-				backWard();
-				// show current folder 
-				listContent($dirPath);
-			}
-			// type of file
-			else {
-				// open the file
-				openFile($dirPath);
-			}
+			echo "No such file or directory!";
+			exit();
 		}
 	?>
 
